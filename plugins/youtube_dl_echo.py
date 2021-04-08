23db314dc8fa6399adb19b2fd8d527b3d74a6ecf
shambhu23 <77667416+shambhu23@users.noreply.github.com>
2021-04-08 12:59:40 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -204,13 +204,13 @@ async def echo(bot, update):
                     # special weird case :\
                     ikeyboard = [
                         InlineKeyboardButton(
-                            "SVideo [" +
+                            "Video [" +
                             "] ( " +
                             approx_file_size + " )",
                             callback_data=(cb_string_video).encode("UTF-8")
                         ),
                         InlineKeyboardButton(
-                            "DFile [" +
+                            "File [" +
                             "] ( " +
                             approx_file_size + " )",
                             callback_data=(cb_string_file).encode("UTF-8")
@@ -240,11 +240,11 @@ async def echo(bot, update):
                 "video", format_id, format_ext)
             inline_keyboard.append([
                 InlineKeyboardButton(
-                    "SVideo",
+                    "Video",
                     callback_data=(cb_string_video).encode("UTF-8")
                 ),
                 InlineKeyboardButton(
-                    "DFile",
+                    "File",
                     callback_data=(cb_string_file).encode("UTF-8")
                 )
             ])
@@ -301,11 +301,11 @@ async def echo(bot, update):
             "video", "OFL", "ENON")
         inline_keyboard.append([
             InlineKeyboardButton(
-                "SVideo",
+                "Video",
                 callback_data=(cb_string_video).encode("UTF-8")
             ),
             InlineKeyboardButton(
-                "DFile",
+                "File",
                 callback_data=(cb_string_file).encode("UTF-8")
             )
         ])
