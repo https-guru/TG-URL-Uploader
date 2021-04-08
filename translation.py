b97b4e58fe717b9ad534464e55a66dbb3c7279bb
shambhu23 <77667416+shambhu23@users.noreply.github.com>
2021-04-08 13:23:28 +0530

    Update translation.py
    Added syntax where it was missing

--- translation.py
+++ translation.py
@@ -7,7 +7,7 @@ This is a Telegram URL Upload Bot!
 /help for more details.."""
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
-    UPGRADE_TEXT = "<b>👉 This bot is already upgraded and totally free🥳</b>
+    UPGRADE_TEXT = "<b>👉 This bot is already upgraded and totally free🥳</b>"
     FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
 URL | filename | username | password"""
